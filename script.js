d5aefed5a36783cbfd85dfd01d20e72c290ce96e
jean <jeansantos@id.uff.br>
2022-02-25 18:19:32 -0300

    incluindo os itens do 12 ao 17

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 // Seletores
-const buttonLogin = document.querySelector('.buttonLogin')
+const buttonLogin = document.querySelector('.buttonLogin');
 
-//Escutadores de Eventos
-buttonLogin.addEventListener('click', checkLogin)
-
-// Funções 
-function checkLogin(event){
- event.preventDefault()
-let email = document.querySelector('#email').value
-let senha = document.querySelector('#senha').value
-checkUserInput(email, senha)
-}
-
-function checkUserInput(email,senha) {
+function checkUserInput(email, senha) {
   if (email === 'tryber@teste.com' && senha === '123456') {
-    alert('Olá, Tryber!')
+    alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos.')
+    alert('Email ou senha inválidos.');
   }
 }
+
+function checkLogin(event) {
+  event.preventDefault();
+  const email = document.querySelector('#email').value;
+  const senha = document.querySelector('#senha').value;
+  checkUserInput(email, senha);
+}
+
+// Escutadores de Eventos
+buttonLogin.addEventListener('click', checkLogin);
